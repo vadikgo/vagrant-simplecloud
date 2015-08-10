9c12f7688ca6d8bfe3683f4d300c3e7a087e21a7
Горбунов Владислав <gorbunov-vv@clodo.ru>
2015-08-10 18:28:27 +0300

    now we can halt and power on vm

--- lib/vagrant-simplecloud/actions/power_on.rb
+++ lib/vagrant-simplecloud/actions/power_on.rb
@@ -10,17 +10,19 @@ module VagrantPlugins
           @app = app
           @machine = env[:machine]
           @client = client
+          @simple_client = simple_client
           @logger = Log4r::Logger.new('vagrant::simplecloud::power_on')
         end
 
         def call(env)
           # submit power on droplet request
-          result = @client.post("/v2/droplets/#{@machine.id}/actions", {
-            :type => 'power_on'
-          })
+          result = JSON.parse(@simple_client.droplet_actions.power_on(droplet_id: @machine.id.to_s))
+          #result = @client.post("/v2/droplets/#{@machine.id}/actions", {
+            #:type => 'power_on'
+          #})
 
           # wait for request to complete
-          env[:ui].info I18n.t('vagrant_simple_cloud.info.powering_on') 
+          env[:ui].info I18n.t('vagrant_simple_cloud.info.powering_on')
           @client.wait_for_event(env, result['action']['id'])
 
           # refresh droplet state with provider
@@ -32,5 +34,3 @@ module VagrantPlugins
     end
   end
 end
-
-
--- lib/vagrant-simplecloud/actions/shut_down.rb
+++ lib/vagrant-simplecloud/actions/shut_down.rb
@@ -10,16 +10,19 @@ module VagrantPlugins
           @app = app
           @machine = env[:machine]
           @client = client
+          @simple_client = simple_client
           @logger = Log4r::Logger.new('vagrant::simplecloud::shut_down')
         end
 
         def call(env)
           # submit shutdown droplet request
-          result = @client.request("/droplets/#{@machine.id}/shutdown")
+          #result = @client.request("/droplets/#{@machine.id}/shutdown")
+          result = JSON.parse(@simple_client.droplet_actions.shutdown(droplet_id: @machine.id.to_s))
 
           # wait for request to complete
           env[:ui].info I18n.t('vagrant_simple_cloud.info.shutting_down')
-          @client.wait_for_event(env, result['event_id'])
+          #@client.wait_for_event(env, result['droplet']['event_id'])
+          @client.wait_for_event(env, result['action']['id'])
 
           # refresh droplet state with provider
           Provider.droplet(@machine, :refresh => true)
@@ -30,4 +33,3 @@ module VagrantPlugins
     end
   end
 end
-
